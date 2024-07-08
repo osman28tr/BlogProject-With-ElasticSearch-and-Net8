27884f851a142599391736e1609912496d5c40b7
OSMAN TONBUL <202804012@ogr.cbu.edu.tr>
2024-07-08 12:23:02 +0300

    Created service and viewmodel class for blog.

---
+++ ElasticSearch.Web/Services/BlogService.cs
@@ -0,0 +1,28 @@
+﻿using ElasticSearch.Web.Models;
+using ElasticSearch.Web.Repositories;
+using ElasticSearch.Web.ViewModels;
+
+namespace ElasticSearch.Web.Services
+{
+    public class BlogService
+    {
+        private readonly BlogRepository _blogRepository;
+        public BlogService(BlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<bool> SaveAsync(CreatedBlogViewModel newBlog)
+        {
+            var blog = new Blog
+            {
+                Title = newBlog.Title,
+                Content = newBlog.Content,
+                Tags = newBlog.Tags.ToArray(),
+                UserId = Guid.NewGuid()
+            };
+            var isCreatedBlog = await _blogRepository.SaveAsync(blog);
+            return isCreatedBlog != null;
+        }
+    }
+}
